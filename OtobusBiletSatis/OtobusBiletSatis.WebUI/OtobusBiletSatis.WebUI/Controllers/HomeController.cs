6bc31eeaf7bb0b19d1447b27dafed0985abda2c4
metehankurnaz <metehan.kurnaz@yahoo.com>
2022-05-13 21:00:30 +0300

    13 mayıs sonu

---
+++ OtobusBiletSatis/OtobusBiletSatis.WebUI/OtobusBiletSatis.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OtobusBiletSatis.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
