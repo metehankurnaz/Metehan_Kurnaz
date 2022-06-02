0ec18e54010d7c2a4e6b37b13c8f1a30a23f6b17
metehankurnaz <metehan.kurnaz@yahoo.com>
2022-06-02 23:02:44 +0300

    .

--- BusTicketReservation/BusTicketReservation.WebUI/Controllers/HomeController.cs
+++ BusTicketReservation/BusTicketReservation.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusTicketReservation.Business.Abstract;
+using BusTicketReservation.Entity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -10,9 +12,54 @@ namespace BusTicketReservation.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        private ICityService _cityService;
+        private IRouteService _routeService;
+        private ITicketService _ticketService;
+        public HomeController(ICityService cityService, IRouteService routeService, ITicketService ticketService)
+        {
+            _cityService = cityService;
+            _routeService = routeService;
+            _ticketService = ticketService;
+        }
         public IActionResult Index()
         {
+            ViewBag.cities = _cityService.GetAll();
             return View();
+        }
+        public IActionResult Ticket(Route route)
+        {
+            return View(_routeService.GetRoutes(route));
+        }
+        public IActionResult Passenger(string FromWhere, string ToWhere, string Date, string Clock, double Price, int RouteId)
+        {
+            var fullseats = _ticketService.GetFullSeats(FromWhere, ToWhere, Date, Clock, Price, RouteId);
+            var seats = new List<int>();
+            for (int i = 1; i < 35; i++)
+            {
+                seats.Add(i);
+            }
+            foreach (var item in fullseats)
+            {
+                seats.Remove(item.SeatNo);
+            }
+
+            ViewBag.seats = seats;
+            Ticket ticket = new Ticket()
+            {
+                FromWhere = FromWhere,
+                ToWhere = ToWhere,
+                Date = Date,
+                Clock = Clock,
+                Price = Price,
+                RouteId = RouteId
+            };
+
+            return View(ticket);
+        }
+        public IActionResult Reserved(Ticket ticket)
+        {
+            _ticketService.CreateTicket(ticket);
+            return View(ticket);
         }
     }
 }
