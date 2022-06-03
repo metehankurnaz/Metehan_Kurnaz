6d2ec09f9030b5d6ee81623544b2c1533a4f1acd
metehankurnaz <metehan.kurnaz@yahoo.com>
2022-06-03 11:55:43 +0300

    03.06.2022

--- MiniShopApp/MiniShopApp.WebUI/Controllers/AdminController.cs
+++ MiniShopApp/MiniShopApp.WebUI/Controllers/AdminController.cs
@@ -37,11 +37,105 @@ namespace MiniShopApp.WebUI.Controllers
         {
             return View(_userManager.Users);
         }
+        public IActionResult UserCreate()
+        {
+            var roles = _roleManager.Roles.Select(i => i.Name);
+            ViewBag.Roles = roles;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UserCreate(UserDetailsModel model, string[] selectedRoles)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    EmailConfirmed = model.EmailConfirmed
+                };
+                var result = await _userManager.CreateAsync(user, "Qwe123.");
+                if (result.Succeeded)
+                {
+                    selectedRoles = selectedRoles ?? new string[] { };
+                    await _userManager.AddToRolesAsync(user, selectedRoles);
+                    return Redirect("~/admin/user/list");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                var roles = _roleManager.Roles.Select(i => i.Name);
+                ViewBag.Roles = roles;
+                return View(model);
+            }
+            var roles2 = _roleManager.Roles.Select(i => i.Name);
+            ViewBag.Roles = roles2;
+            return View(model);
+        }
+        public async Task<IActionResult> UserEdit(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                var selectedRoles = await _userManager.GetRolesAsync(user);
+                var roles = _roleManager.Roles.Select(i => i.Name);
+                ViewBag.Roles = roles;
+                return View(new UserDetailsModel() 
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.Email,
+                    EmailConfirmed = user.EmailConfirmed,
+                    SelectedRoles = selectedRoles
+                });
+            }
+            return Redirect("~/admin/user/list");
+        }
+        [HttpPost]
+        public async Task<IActionResult> UserEdit(UserDetailsModel model, string[] selectedRoles)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(model.UserId);
+                if (user != null)
+                {
+                    user.FirstName = model.FirstName;
+                    user.LastName = model.LastName;
+                    user.UserName = model.UserName;
+                    user.Email = model.Email;
+                    user.EmailConfirmed = model.EmailConfirmed;
+                }
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    var userRoles = await _userManager.GetRolesAsync(user);
+                    selectedRoles = selectedRoles ?? new string[] { };
+                    await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles).ToArray<string>());
+                    await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles).ToArray<string>());
+                    return Redirect("~/admin/user/list");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                var roles2 = _roleManager.Roles.Select(i => i.Name);
+                ViewBag.Roles = roles2;
+                return View(model);
+            }
+            ModelState.AddModelError("", "Lütfen ilgili alanları kontrol ediniz!");
+            var roles = _roleManager.Roles.Select(i => i.Name);
+            ViewBag.Roles = roles;
+            return View(model);
+        }
         public IActionResult RoleList()
         {
             return View(_roleManager.Roles);
         }
-
         public IActionResult RoleCreate()
         {
             return View();
--- MiniShopApp/MiniShopApp.WebUI/Startup.cs
+++ MiniShopApp/MiniShopApp.WebUI/Startup.cs
@@ -93,7 +93,7 @@ namespace MiniShopApp.WebUI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
         {
             if (env.IsDevelopment())
             {
@@ -123,6 +123,24 @@ namespace MiniShopApp.WebUI
                     );
 
                 endpoints.MapControllerRoute(
+                    name: "adminusercreate",
+                    pattern: "admin/user/create",
+                    defaults: new { controller = "Admin", action = "UserCreate" }
+                    );
+
+                endpoints.MapControllerRoute(
+                    name: "adminrolecreate",
+                    pattern: "admin/role/create",
+                    defaults: new { controller = "Admin", action = "RoleCreate" }
+                    );
+
+                endpoints.MapControllerRoute(
+                    name: "adminuseredit",
+                    pattern: "admin/user/{id}",
+                    defaults: new { controller = "Admin", action = "UserEdit" }
+                    );
+
+                endpoints.MapControllerRoute(
                     name: "adminroles",
                     pattern: "admin/role/list",
                     defaults: new { controller = "Admin", action = "RoleList" }
@@ -134,11 +152,7 @@ namespace MiniShopApp.WebUI
                     defaults: new { controller = "Admin", action = "RoleEdit" }
                     );
 
-                endpoints.MapControllerRoute(
-                    name: "adminrolecreate",
-                    pattern: "admin/role/create",
-                    defaults: new { controller = "Admin", action = "RoleCreate" }
-                    );
+                
 
 
 
@@ -177,6 +191,8 @@ namespace MiniShopApp.WebUI
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            //Buraya kullanýcý bilgilerini oluþturacak metodumuzu çaðýran kodu yazacaðýz.
+            SeedIdentity.Seed(userManager, roleManager, Configuration).Wait();
         }
     }
 }
