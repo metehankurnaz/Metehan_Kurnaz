0b7f8030bf9a5629efcfa0da9a8e9b03c9b610de
metehankurnaz <metehan.kurnaz@yahoo.com>
2022-06-20 23:53:32 +0300

    20.06.2022

---
+++ BusTicketReservation/BusTicketReservation.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using BusTicketReservation.WebUI.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusTicketReservation.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BusTicketReservation/BusTicketReservation.WebUI/Identity/SeedIdentity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusTicketReservation.WebUI.Identity
+{
+    public class SeedIdentity
+    {
+        //public static async Task Seed(UserManager<User> userManager, RoleManager<IdentityRole> roleManager, ICardManager cardService, IConfiguration configuration)
+        //{
+
+        //}
+    }
+}
